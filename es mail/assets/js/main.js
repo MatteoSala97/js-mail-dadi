10c043c1afff83ed92b516c68b7c689aa5af7146
MatteoSala97 <matteo111997@gmail.com>
2024-01-10 22:11:42 +0100

    feat: js eventlistener for submit button

--- es mail/assets/js/main.js
+++ es mail/assets/js/main.js
@@ -9,3 +9,20 @@ for (let i = 0; i < emailList.length; i++){
     console.log(emailList[i])
 }
 
+// creating an event listener on click and subsequent function
+
+document.querySelector('form').addEventListener('submit', function (e) {
+    // getting email const from input and making it into value
+    const enteredEmail = emailHtml.value
+
+    //this prevents the page from reloading after pressing the button
+    e.preventDefault()
+
+    // function that checks if the entered email is in the email list
+    if (emailList.includes(enteredEmail)) {
+        resultMessage.innerHTML = "<p class='text-light bg-success'>You are part of the crew!</p>"
+    } else {
+        resultMessage.innerHTML = "<p class='text-light bg-danger'>You are not part of the crew!</p>"
+    }
+    
+});
