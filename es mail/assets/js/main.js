c1826bb33e8350f75fa748364b43a2526ead972f
MatteoSala97 <matteo111997@gmail.com>
2024-01-10 21:54:12 +0100

    feat: js array of emails

---
+++ es mail/assets/js/main.js
@@ -0,0 +1,11 @@
+const emailHtml = document.getElementById("email")
+// const userEmail = window.alert("Please enter your email")
+
+
+//creating an array of emails   
+const emailList = [ "vladimir.ivanov@secretbaseunderthesea.com", "sasha.kozlov@secretbaseunderthesea.com", "sergei.sokolov@secretbaseunderthesea.com", "lukyan.goluvbev@secretbaseunderthesea.com", "misha.vasyliev@secretbaseunderthesea.com" ]
+
+for (let i = 0; i < emailList.length; i++){
+    console.log(emailList[i])
+}
+
